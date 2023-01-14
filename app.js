9546d25ae732834fff358fcf041246fe4ba124c4
GauravBarua <113329451+GauravBarua@users.noreply.github.com>
2023-01-14 14:55:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const config = require('./config/config')
 const port = process.env.PORT || 3000
 const app = config.setUpServer()
 
+Route: /auth
+Method: POST,
+AdminPrivilege: false,
+Header: --,
+Body: {
+        email: teste@teste.com,
+        password: "teste"
+    }
+
 app.listen(port, () => {
   console.log(` ----------------------\n|      SERVER ON!      |\n|      Porta: ${port}     |\n ----------------------`)
 })
